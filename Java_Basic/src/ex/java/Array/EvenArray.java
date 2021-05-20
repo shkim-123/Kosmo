b375c3ac471e515cff8cf36e560e3238e0584c9c
shkim-123 <82145134+shkim-123@users.noreply.github.com>
2021-05-20 16:19:25 +0900

    Create EvenArray.java

---
+++ Java_Basic/src/ex/java/Array/EvenArray.java
@@ -0,0 +1,38 @@
+package ex.java.Array;
+
+import java.util.Scanner;
+
+public class EvenArray {
+
+	public static void main(String[] args) {
+		// [-12, 3, -9, 5, 8, -2, 0, -8, 3, 10] 정수 배열을 입력 받음
+		// while문 이용하여 짝수 번째 요소 합과 평균 출력
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("정수 배열을 입력해 주세요. 구분자 \", \"");
+		String sc = scanner.nextLine();
+		
+		String[] num = sc.split(", ");
+		
+		int i = 0;
+		int sum = 0;
+		int avg = 0;
+		int cnt = 0;
+		
+		while(i < num.length) {
+			if(Integer.parseInt(num[i]) % 2 == 0) {
+				sum += Integer.parseInt(num[i]);
+				cnt++;
+			}
+			i++;
+		}
+		
+		avg = sum/cnt;
+		
+		System.out.println("합산 : " + sum);
+		System.out.println("평균 : " + avg);
+		
+	}
+
+}
