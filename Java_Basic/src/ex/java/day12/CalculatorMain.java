6e6654bf824e36f01145c89967aa2764385b993e
shkim-123 <82145134+shkim-123@users.noreply.github.com>
2021-05-28 09:56:16 +0900

    210528 인터페이스 직접 구현(익명 구현 객체)

--- Java_Basic/src/ex/java/day12/CalculatorMain.java
+++ Java_Basic/src/ex/java/day12/CalculatorMain.java
@@ -13,6 +13,35 @@ public class CalculatorMain {
 		System.out.println(calc.times(num1, num2));
 		System.out.println(calc.divide(num1, num2));
 		calc.showInfo();
+		
+		// 인터페이스를 직접 구현하여 사용, 코드 블록 끝에 ; 추가
+		Calc c = new Calc() {
+
+			@Override
+			public int add(int num1, int num2) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public int substract(int num1, int num2) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public int times(int num1, int num2) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public int divide(int num1, int num2) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+			
+		};
 
 	}
 
