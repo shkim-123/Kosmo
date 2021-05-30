cd0807506cbcd7fcfe29822e119f466723f44b3e
SOHEE\Kim Sohee <piia_@naver.com>
2021-05-30 20:36:42 +0900

    210530 final 상수

---
+++ Java_Basic/src/ex/java/day12/Define.java
@@ -0,0 +1,8 @@
+package ex.java.day12;
+
+public class Define {
+	public static final int MIN = 1;
+	public static final int MAX = 9999;
+	public static final int ENG = 1001;
+	public static final String GOOD_MORNING = "Good Morning!";
+}
