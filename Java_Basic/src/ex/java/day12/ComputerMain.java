5571bdf512995bdec221371227eeec4f512fff8c
SOHEE\Kim Sohee <piia_@naver.com>
2021-05-30 20:27:55 +0900

    210530 추상메서드

--- Java_Basic/src/ex/java/day12/ComputerMain.java
+++ Java_Basic/src/ex/java/day12/ComputerMain.java
@@ -4,7 +4,7 @@ public class ComputerMain {
 
 	public static void main(String[] args) {
 		
-		// 추상 클래스는 객체 생성 ㅂ루가
+		// 추상 클래스는 객체 생성 불가
 		// Computer c1 = new Computer();
 		Computer c2 = new DeskTop();
 		
