950bd43c05a59b2a59ec4148f315b11166d9c2c8
SOHEE\Kim Sohee <piia_@naver.com>
2021-05-31 15:20:38 +0900

    210531 LinkedList

---
+++ Java_Basic/src/ex/java/day14/LinkedListTest.java
@@ -0,0 +1,28 @@
+package ex.java.day14;
+
+import java.util.LinkedList;
+
+public class LinkedListTest {
+
+	public static void main(String[] args) {
+		
+		LinkedList<String> myList = new LinkedList<String>();
+		
+		myList.add("A");
+		myList.add("B");
+		myList.add("C");
+		
+		System.out.println(myList);
+		
+		myList.add(1, "D");
+		System.out.println(myList);
+		
+		myList.addFirst("0");
+		System.out.println(myList);
+		
+		System.out.println(myList.removeLast());
+		System.out.println(myList);
+
+	}
+
+}
