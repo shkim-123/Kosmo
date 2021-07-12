b9cb04db412c74bcc66a6805a3b1b410d0fde3f8
SH Kim <82145134+shkim-123@users.noreply.github.com>
2021-07-12 12:58:50 +0900

    Add files via upload

--- Frontend/common_func.js
+++ Frontend/common_func.js
@@ -12,45 +12,31 @@ function isEmpty(str) {
 // 한글 이름 유효성 체크
 // 한글 2자 ~ 20자 입력
 // 유효성 체크 통과 시 true, 아니면 false 리턴
-function isMem_name(str){
-	var flag = false;
+function isMem_name(mem_name){
 	var regExp = new RegExp(/^[가-힣]{2,20}$/);
-
-	// 유효성 체크 통과 시
-	if(regExp.test(str)) {
-		flag = true;
-	}
-
-	return flag;
+	return regExp.test(mem_name);
 }
 
 
 // 아이디 유효성 체크
 // 첫글자는 영소문자 입력하고, 이후 영소문자 숫자 _ 만 입력가능. 5자~10자 입력
 // 유효성 체크 통과 시 true, 아니면 false 리턴
-function isUid(str) {
-	var flag = false;
+function isUid(uid) {
 	var regExp = new RegExp(/^[a-z][a-z0-9_]{4,9}$/);
-
-	// 유효성 체크 통과 시
-	if(regExp.test(str)) {
-		flag = true;
-	}
-
-	return flag;
+	return regExp.test(uid);
 }
 
 // 비밀번호 유효성 체크
 // 영소문자, 숫자, _ 만 입력, 5 ~ 8자 입력
 // 유효성 체크 통과 시 true, 아니면 false 리턴
-function isPwd(str) {
-	var flag = false;
+function isPwd(pwd) {
 	var regExp = new RegExp(/^[a-z0-9_]{5,8}$/);
+	return regExp.test(pwd);
+}
 
-	//유효성 체크 통과 시
-	if(regExp.test(str)) {
-		flag = true;
-	}
-
-	return flag;
+// 이메일 유효성 체크
+// 유효성 체크 통과 시 true, 아니면 false 리턴
+function isEmail(email) {
+	var regExp = new RegExp(/^([0-9a-zA-Z_-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/);
+	return regExp.test(email);
 }
